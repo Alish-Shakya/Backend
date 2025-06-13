668810028401a14932323ded2f00c5a8367d38a2
Alish-Shakya <alishshakya44@gmail.com>
2025-06-13 19:57:36 +0545

    inital commit

---
+++ src/Controller/userController.js
@@ -0,0 +1,15 @@
+const createUser = async (req, res, next) => {
+  try {
+    let result = await User.create(req.body);
+    res.status(201).json({
+      success: true,
+      message: "user created successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
---
+++ src/Schema/userSchema.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+export default userSchema;
