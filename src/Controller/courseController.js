98987c9de41ae4183bf39081ea5db3bc92e686cf
Alish-Shakya <alishshakya44@gmail.com>
2025-06-20 12:07:18 +0545

    all cources are readed

--- src/Controller/courseController.js
+++ src/Controller/courseController.js
@@ -2,10 +2,26 @@ import { Course } from "../Model/modol.js";
 
 export const createCourse = async (req, res, next) => {
   try {
-    let result = await Course(req.body);
+    let result = await Course.create(req.body);
     res.status(200).json({
       success: true,
       message: "Course created successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const readAllCourse = async (req, res, next) => {
+  try {
+    let result = await Course.find({});
+    res.status(200).json({
+      success: true,
+      message: "All courses retrived",
       data: result,
     });
   } catch (error) {
--- src/Routes/courseRoute.js
+++ src/Routes/courseRoute.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createCourse } from "../Controller/courseController.js";
+import { createCourse, readAllCourse } from "../Controller/courseController.js";
 
 const courseRouter = Router();
 
 courseRouter.route("/createCourse").post(createCourse);
+courseRouter.route("/allCourse").get(readAllCourse);
 
 export default courseRouter;
